2f1aa7c2695bce2bcc2e4eee82b76109902691a5
a21mj0n <azimjontoirov@gmail.com>
2020-12-25 14:57:11 +0500

    get zapros

--- app.js
+++ app.js
@@ -27,16 +27,16 @@ routes.forEach((item) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express(path.join(__dirname, 'client', 'dist', 'spa')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'spa', 'index.html'));
+  app.use('/', express.static('client/dist/spa'));
+  app.get('*/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/dist/spa', 'index.html'));
   });
 }
 
-app.use('/', express(path.join(__dirname, 'client', 'dist', 'spa')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'spa', 'index.html'));
-});
+// app.use('/', express(path.join(__dirname, 'client', 'dist', 'spa')));
+// app.get('*/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/dist/spa', 'index.html'));
+// });
 
 
 // init app
