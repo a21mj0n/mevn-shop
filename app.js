8d620eed0dc4509077d60ee34daa90b2fc8c132c
a21mj0n <azimjontoirov@gmail.com>
2020-12-25 15:16:55 +0500

    refactored app.js 2

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ routes.forEach((item) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static('client/dist/spa'));
+  app.use(express.static('client/dist/spa'));
 }
 
-app.get('*/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/dist/spa', 'index.html'));
 });
 
